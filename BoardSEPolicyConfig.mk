13fc6b602e6daf7d2bfc3c48e781253bf8074d50
TheMalachite <eliasgheeraert@gmail.com>
2022-01-26 19:12:24 +0100

    mtk-sepolicy: Initial SEPolicy rules
    Change-Id: Idca3946c2b16ee87becfbe8cd2d7e148c39bc1f1

---
+++ BoardSEPolicyConfig.mk
@@ -0,0 +1,19 @@
+# SELinux Policy File Configuration
+BOARD_SEPOLICY_DIRS += \
+    device/mediatek/sepolicy/basic/non_plat \
+    device/mediatek/sepolicy/bsp/non_plat
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/mediatek/sepolicy/basic/plat_private \
+    device/mediatek/sepolicy/bsp/plat_private
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/mediatek/sepolicy/basic/plat_public \
+    device/mediatek/sepolicy/bsp/plat_public
+
+# MTK Neverallow Rules Configuration
+ifneq (yes,$(strip $(MTK_BUILD_WITHOUT_MTK_NEVERALLOW_RULES)))
+BOARD_SEPOLICY_DIRS += device/mediatek/sepolicy/basic/neverallows/non_plat
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/mediatek/sepolicy/basic/neverallows/plat_public
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/mediatek/sepolicy/basic/neverallows/plat_private
+endif
