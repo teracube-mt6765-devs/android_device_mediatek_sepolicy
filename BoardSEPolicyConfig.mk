91d52bb2df2f19e8f102e382fbdef3c69b6dab42
Gagan Malvi <malvigagan@gmail.com>
2022-06-09 19:21:22 +0200

    sepolicy: Don't build neverallows and move to SYSTEM_EXT_*
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- BoardSEPolicyConfig.mk
+++ BoardSEPolicyConfig.mk
@@ -8,17 +8,10 @@ BOARD_SEPOLICY_DIRS += \
     device/mediatek/sepolicy/$(TARGET_BOARD_PLATFORM)/full \
     device/mediatek/sepolicy/modem
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     device/mediatek/sepolicy/basic/plat_private \
     device/mediatek/sepolicy/bsp/plat_private
 
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
     device/mediatek/sepolicy/basic/plat_public \
     device/mediatek/sepolicy/bsp/plat_public
-
-# MTK Neverallow Rules Configuration
-ifneq (yes,$(strip $(MTK_BUILD_WITHOUT_MTK_NEVERALLOW_RULES)))
-BOARD_SEPOLICY_DIRS += device/mediatek/sepolicy/basic/neverallows/non_plat
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/mediatek/sepolicy/basic/neverallows/plat_public
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/mediatek/sepolicy/basic/neverallows/plat_private
-endif
