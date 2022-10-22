2ae27f98ac8f04387a5fbb2741571960bbe58e8d
Gagan Malvi <malvigagan@gmail.com>
2022-10-22 17:03:29 +0200

    sepolicy: Update to Android 12 release
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- BoardSEPolicyConfig.mk
+++ BoardSEPolicyConfig.mk
@@ -3,15 +3,31 @@
 BOARD_SEPOLICY_DIRS += \
     device/mediatek/sepolicy/basic/non_plat \
     device/mediatek/sepolicy/bsp/non_plat \
-    device/mediatek/sepolicy/$(TARGET_BOARD_PLATFORM)/basic \
-    device/mediatek/sepolicy/$(TARGET_BOARD_PLATFORM)/bsp \
-    device/mediatek/sepolicy/$(TARGET_BOARD_PLATFORM)/full \
     device/mediatek/sepolicy/modem
 
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+ifneq ($(call math_lt,$(PRODUCT_SHIPPING_API_LEVEL),28),)
+BOARD_SEPOLICY_DIRS += $(wildcard device/mediatek/sepolicy/bsp/ota_upgrade)
+endif
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/mediatek/sepolicy/basic/plat_private \
     device/mediatek/sepolicy/bsp/plat_private
 
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
     device/mediatek/sepolicy/basic/plat_public \
     device/mediatek/sepolicy/bsp/plat_public
+
+# MTK Debug Rules Configuration
+ifeq ($(strip $(HAVE_MTK_DEBUG_SEPOLICY)), yes)
+BOARD_SEPOLICY_DIRS += \
+    device/mediatek/sepolicy/basic/debug/non_plat \
+    device/mediatek/sepolicy/bsp/debug/non_plat
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/mediatek/sepolicy/basic/debug/plat_public \
+    device/mediatek/sepolicy/bsp/debug/plat_public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/mediatek/sepolicy/basic/debug/plat_private \
+    device/mediatek/sepolicy/bsp/debug/plat_private
+endif
