a1fb68bdc2b2f1a705f908c868f477032d732cf1
SahilSonar <sss.sonar2003@gmail.com>
2022-10-28 11:45:05 +0200

    mtk-sepolicy: Switch to SYSTEM_EXT_{PUBLIC,PRIVATE}_SEPOLICY_DIRS
    Fixes:
    warning: BOARD_PLAT_PRIVATE_SEPOLICY_DIR has been deprecated.
        Use SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS instead.
    warning: BOARD_PLAT_PUBLIC_SEPOLICY_DIR has been deprecated.
        Use SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS instead.

--- BoardSEPolicyConfig.mk
+++ BoardSEPolicyConfig.mk
@@ -9,11 +9,11 @@ ifneq ($(call math_lt,$(PRODUCT_SHIPPING_API_LEVEL),28),)
 BOARD_VENDOR_SEPOLICY_DIRS += $(wildcard device/mediatek/sepolicy/bsp/ota_upgrade)
 endif
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     device/mediatek/sepolicy/basic/plat_private \
     device/mediatek/sepolicy/bsp/plat_private
 
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
     device/mediatek/sepolicy/basic/plat_public \
     device/mediatek/sepolicy/bsp/plat_public
 
@@ -23,11 +23,11 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
     device/mediatek/sepolicy/basic/debug/non_plat \
     device/mediatek/sepolicy/bsp/debug/non_plat
 
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
     device/mediatek/sepolicy/basic/debug/plat_public \
     device/mediatek/sepolicy/bsp/debug/plat_public
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     device/mediatek/sepolicy/basic/debug/plat_private \
     device/mediatek/sepolicy/bsp/debug/plat_private
 endif
